26385276aa0c650c2a0f853aff851537747d9921
gregsinnett <gsinnett@gmail.com>
2023-11-21 08:32:25 -0800

    test vertical momentum and nonhydrostatic model

--- upwelling/UW_analysis.jl
+++ upwelling/UW_analysis.jl
@@ -1,14 +1,31 @@
-using JLD2
+using OceanBioME, Oceananigans, Printf
+using Oceananigans.Units
 
-# Load the output file
-output_file_path = "eady_turbulence_bgc.jld2"
-output_data = load(output_file_path)
+# Now load the saved output,
+ζ = FieldTimeSeries("eady_turbulence_bgc.jld2", "ζ")
+P = FieldTimeSeries("eady_turbulence_bgc.jld2", "P")
+NO₃ = FieldTimeSeries("eady_turbulence_bgc.jld2", "NO₃")
+NH₄ = FieldTimeSeries("eady_turbulence_bgc.jld2", "NH₄")
+DIC = FieldTimeSeries("eady_turbulence_bgc.jld2", "DIC")
 
-# Extract variables of interest
-ζ = output_data["ζ"]
-P = output_data["P"]
-NO₃ = output_data["NO₃"]
-NH₄ = output_data["NH₄"]
-DIC = output_data["DIC"]
+times = ζ.times
 
-# You can now perform analysis on these variables as needed.
+xζ, yζ, zζ = nodes(ζ)
+xc, yc, zc = nodes(P)
+nothing #hide
+
+# Get things to plot
+
+
+# Example plot
+using CairoMakie
+
+fig = Figure(resolution = (1000, 1500), fontsize = 20)
+
+axis_kwargs = (xlabel = "Time (days)", ylabel = "z (m)", limits = ((0, times[end] / days), (-100meters, 0)))
+
+axP = Axis(fig[1, 1]; title = "Phytoplankton concentration (mmol N / m³)", axis_kwargs...)
+hmP = heatmap!(times / days, zc, interior(P, 1, 1, :, :)', colormap = :batlow)
+Colorbar(fig[1, 2], hmP)
+
+fig
